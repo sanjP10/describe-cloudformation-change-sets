c4e5603125b2c78b9f1e615df93072de8d1e1c75
SanjayPatel <sanjay10.patel@gmail.com>
2022-01-09 15:41:40 +0000

    change template_body to template_file, add changelog, update readme

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 1.0.0
+
+*  Initial action to describe change sets and return html string format of changes
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 has_changes="false"
 # Create empty stack in review in progress stage w/ change set or create change set for existing stack
-aws cloudformation deploy --stack-name "$INPUT_STACK_NAME" --template-file <location of cloudformation file>  --no-fail-on-empty-changeset --no-execute-changeset "$3"
+aws cloudformation deploy --stack-name "$INPUT_STACK_NAME" --template-file "$INPUT_TEMPLATE_FILE"  --no-fail-on-empty-changeset --no-execute-changeset "$3"
 
 change_set_name=$(aws cloudformation list-change-sets --stack-name "$INPUT_STACK_NAME" | jq .Summaries[0].ChangeSetName | tr -d '"')
 # If change_set_name is not null, describe and put output to a json file. Delete the change set and process the json
