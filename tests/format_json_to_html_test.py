df98d2ebad7d7a6c178521e8ad4461d1d9a2f92e
Sanjay Patel <sanjay10.patel@gmail.com>
2022-01-09 17:00:58 +0000

    fix linting

--- tests/format_json_to_html_test.py
+++ tests/format_json_to_html_test.py
@@ -7,30 +7,39 @@ from src.format_json_to_html import execute
 
 class TestFormattingOfChangeSetDescribes(unittest.TestCase):
     """ Unit tests for formatting reports """
+
     def test_creation_deletion(self):
         """ Creations and deletions """
-        result = execute('describe-add-remove', 'fixtures/descriptions/describe-add-remove.json', '')
+        result = execute('describe-add-remove',
+                         'fixtures/descriptions/describe-add-remove.json',
+                         '')
         with open('fixtures/html/add-remove.html', 'rt', encoding='UTF-8') as file:
             data = file.read().rstrip()
             self.assertEqual(result, data)
 
     def test_changes(self):
         """ Changes """
-        result = execute('describe-changes', 'fixtures/descriptions/describe-changes.json', '')
+        result = execute('describe-changes',
+                         'fixtures/descriptions/describe-changes.json',
+                         '')
         with open('fixtures/html/changes.html', 'rt', encoding='UTF-8') as file:
             data = file.read().rstrip()
             self.assertEqual(result, data)
 
     def test_no_changes(self):
         """ No Changes """
-        result = execute('describe-no-changes', 'fixtures/descriptions/describe-no-changes.json', '')
+        result = execute('describe-no-changes',
+                         'fixtures/descriptions/describe-no-changes.json',
+                         '')
         with open('fixtures/html/no-change.html', 'rt', encoding='UTF-8') as file:
             data = file.read().rstrip()
             self.assertEqual(result, data)
 
     def test_changes_with_env(self):
         """ Changes with environment name """
-        result = execute('describe-changes', 'fixtures/descriptions/describe-changes.json', 'dev')
+        result = execute('describe-changes',
+                         'fixtures/descriptions/describe-changes.json',
+                         'dev')
         with open('fixtures/html/changes-with-env.html', 'rt', encoding='UTF-8') as file:
             data = file.read().rstrip()
             self.assertEqual(result, data)
